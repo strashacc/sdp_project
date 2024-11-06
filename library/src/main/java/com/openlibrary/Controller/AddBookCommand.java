00c1f7cd200ec77854b3cb4bf48652256bcf1a38
Baglan-maker <bagl203456@gmail.com>
2024-11-06 22:00:14 +0500

    Update AddBookCommand.java

--- library/src/main/java/com/openlibrary/Controller/AddBookCommand.java
+++ library/src/main/java/com/openlibrary/Controller/AddBookCommand.java
@@ -10,30 +10,21 @@ import com.openlibrary.Model.BookService;
 public class AddBookCommand implements Command {
     private BookService bookService;
     private Book book;
-    public static Book lastAddedBook;
-
 
     public AddBookCommand(BookService bookService, Book book) {
         this.bookService = bookService;
         this.book = book;
-        lastAddedBook = book; // Keep track of the last added book
     }
 
     public AddBookCommand(BookService bookService, String type, int id, String title, String author, String imgLink) {
         this.bookService = bookService;
         this.book = BookFactory.createBook(type, id, title, author, imgLink);
-        lastAddedBook = this.book; // Keep track of the last added book
     }
-
-
 
     @Override
     public void execute() {
         bookService.addBook(book);
     }
-   
-
-
     }
 
 
